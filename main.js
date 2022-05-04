c1dfde27b6643c725980d7c0798476bdc7991701
Michelle Ahazie <ahazie.michelle@gmail.com>
2022-05-04 16:56:21 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+// Write a function that takes a number and returns true if it is a positive number and false if it is a negative number.
+
+const isNumberPositive = (number) => {
+if (number >= 0) {
+return ("true") 
+}
+if (number <= 0) {
+return ("false")
+}
+};
+
+console.log(isNumberPositive(2))
+
+//  Write a function that takes a number of days and converts it into an age.
+
+// Write a function that takes three numbers and returns the largest of the three numbers.
+
+// Write a function that takes an array of names and returns the last name from the array of names.
+
+// Write a function that takes an array of numbers and returns true if all of the numbers are positive. It should return false if there are one or more negative numbers in the array.
