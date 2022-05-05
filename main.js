c2daa24a031521494767dcc45759df6ade232481
Michelle Ahazie <ahazie.michelle@gmail.com>
2022-05-05 15:08:37 +0100

    made changes

--- main.js
+++ main.js
@@ -2,14 +2,14 @@
 
 const isNumberPositive = (number) => {
 if (number >= 0) {
-return ("true") 
+return console.log("true")
 }
 if (number <= 0) {
-return ("false")
+return console.log("false")
 }
-};
+}
 
-isNumberPositive(2)
+isNumberPositive(2);
 
 //  Write a function that takes a number of days and converts it into an age.
 
@@ -23,7 +23,10 @@ numberToAge(2500);
 // Write a function that takes three numbers and returns the largest of the three numbers.
 
 const largestNumber = (number1, number2, number3) => {
-    return Math.max(number1, number2, number3)
+    const numberOfLargest = Math.max(number1, number2, number3)
+    console.log(numberOfLargest)
+    return numberOfLargest;
+    
 }
 
 largestNumber(100,15,30)
@@ -43,15 +46,28 @@ returnLastName(names);
 
 // Write a function that takes an array of numbers and returns true if all of the numbers are positive. It should return false if there are one or more negative numbers in the array.
 
-let listOfNumbers = [1,2,-3,4,5]
+let listOfNumbers = [1,-2,3,4,5]
+
+// const arrayOfNumber = (numbers) => {
+//     numbers.forEach(number => {
+//         if (number >= 0) {
+//             return console.log(true)
+//         } else {
+//             return console.log(false)
+//         }
+//     })
+// }
+// arrayOfNumber(listOfNumbers)
 
 const arrayOfNumber = (numbers) => {
-    numbers.forEach(number => {
-        if (number >= 0) {
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] >= 0) {
             return console.log(true)
         } else {
             return console.log(false)
         }
-    })
+    }
 }
 arrayOfNumber(listOfNumbers)
+
+
